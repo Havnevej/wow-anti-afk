e97817373cf89bd1d4ca4a88e301b9bc1b769eab
Anton D <you@example.com>
2022-10-23 13:33:14 +0200

    Initial add

---
+++ anti-afk.py
@@ -0,0 +1,60 @@
+import re
+import pyautogui
+from win32gui import GetWindowText, GetForegroundWindow, EnumWindows, SetForegroundWindow
+import time
+import random
+
+wowGuid=""
+currentGuid=""
+
+def sendWowKeyPress(key, delay=0.05, modifier=None):
+    if "+" in key:
+        keyFrag = key.split("+")
+        modifier = keyFrag[0]
+        key = keyFrag[1]
+    if modifier:
+        pyautogui.keyDown(modifier)
+    pyautogui.keyDown(key)
+    time.sleep(delay)
+    pyautogui.keyUp(key)
+    if modifier:
+        pyautogui.keyUp(modifier)
+    
+def windowsCallback(windowid, name):
+    #Find wow window handle
+    global wowGuid
+    if re.match(name, str(GetWindowText(windowid))) is not None:
+        wowGuid=windowid
+        
+
+def main():
+    global wowGuid,currentGuid
+    print("Loaded... watching for WoW window and sending kess presses when wow window active")
+    wowTitle = "World of Warcraft"
+    keys = ['a', 'd', 'space', 'alt+1', 'q']
+    nextKey=''
+    while True:
+        currentGuid=GetForegroundWindow()
+        EnumWindows(windowsCallback, wowTitle)
+        
+        if(currentGuid != wowGuid):
+            SetForegroundWindow(wowGuid)
+            
+        if nextKey != '':
+            k=nextKey
+            nextKey=''
+        else:
+            k=random.choice(keys)
+            if k == 'a':
+                nextKey = 'd'
+            elif k == 'd':
+                nextKey = 'a'
+        sendWowKeyPress(k)
+        print("pressing key: " + k)
+        SetForegroundWindow(currentGuid)
+
+        interval = random.randint(60, 600)
+        time.sleep(interval)
+
+if __name__ == "__main__":
+    main()
